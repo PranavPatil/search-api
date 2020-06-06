c009e3de30cb06356393f9a25e09973a1ab9833e
Pranav Patil <pranav.java2@gmail.com>
2020-06-06 00:54:41 -0500

    Adding search-api codebase

---
+++ src/main/java/com/library/api/search/engine/google/Image.java
@@ -0,0 +1,14 @@
+package com.library.api.search.engine.google;
+
+public class Image {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String u) {
+        this.url = u;
+    }
+}
